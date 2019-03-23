e348f7f5ce0683c3647e14230247ae594e512886
AlexPatrin <luiziana2012@mail.ru>
2019-03-23 14:27:21 +0300

    Les01 finished

--- Les01.py
+++ Les01.py
@@ -1,1 +1,12 @@
-print("First programm on geekBrains")
+print("First programm on GeekBrains")
+name = input("Your name: ")
+print("Hello ",  name)
+answer=input("Lets coding? (Y/N)")
+
+if answer =="Y":
+	pass
+	print("You Win")
+elif answer == "N":
+	print("bye bye")
+else:
+	print("Se you late")
